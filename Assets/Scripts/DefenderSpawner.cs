588082a1b3d68c8591e5b15510b60ed6cb397621
ValverdeDaniel <41589992+ValverdeDaniel@users.noreply.github.com>
2020-07-28 09:42:11 -0600

    initial commit gardentd

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    Defender defender;
+    public void OnMouseDown()
+    {
+
+        SpawnDefender(GetSquareClicked());
+    }
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //creating screen to world point so it clicks on the grid
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        //remove this to place them anywhere and remove SnapToGrid
+        Vector2 gridPos = SnapToGrid(worldPos);
+        //change this to worldPos to place them anywhere
+        return gridPos;
+    }
+
+    //remove SnapToGrid if you wanna place them anywhere
+    private Vector2 SnapToGrid(Vector2 roundedPos)
+    {
+        float newX = Mathf.RoundToInt(roundedPos.x);
+        float newY = Mathf.RoundToInt(roundedPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 worldPos)
+    {
+        Defender newDefender = Instantiate(defender, worldPos, Quaternion.identity) as Defender;
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotationSpeed = 360f;
+    [SerializeField] float damage = 50;
+
+    //[SerializeField] float speedOfSpin = 360f;
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeed, 0);
+    }
+
+    void Update()
+    {
+        //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        //transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+
+        transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+        
+        if(attacker && health)
+        {            
+            //reduce health
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+
+    }
+
+}
