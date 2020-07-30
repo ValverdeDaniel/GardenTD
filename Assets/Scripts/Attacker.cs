3560ff5c3a93c30ac5c74cb78615ff5b46b6b11a
ValverdeDaniel <41589992+ValverdeDaniel@users.noreply.github.com>
2020-07-30 09:38:09 -0600

    defenders and attackers attack eachother and it all works alright

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,10 +7,20 @@ public class Attacker : MonoBehaviour
 
     [Range(0f, 5f)]
     float currentSpeed = 1f;
+    GameObject currentTarget;
     
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if(!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
@@ -18,7 +28,21 @@ public class Attacker : MonoBehaviour
         currentSpeed = speed;
     }
 
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
 
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+    }
 
     private void Die()
     {
