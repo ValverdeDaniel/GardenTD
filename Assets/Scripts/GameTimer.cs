1094f3c84f8e7acfccf391d67f8ef0d802d80b76
ValverdeDaniel <41589992+ValverdeDaniel@users.noreply.github.com>
2020-07-30 13:12:53 -0600

    stop attacker spawner and win game

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -7,16 +7,24 @@ public class GameTimer : MonoBehaviour
 {
     [Tooltip("Our level timer in Seconds")]
     [SerializeField] float levelTime = 10;
+    bool triggeredLevelFinished = false;
 
     // Update is called once per frame
     void Update()
     {
+        //this mean if triggeredlevelFinished == true
+        if (triggeredLevelFinished)
+        {
+            return;
+        }
+
         GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
 
         bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
         if (timerFinished)
         {
-            Debug.Log("level timer expired");
+            FindObjectOfType<LevelController>().LevelTimerFinished();
+            triggeredLevelFinished = true;
         }
     }
 }
